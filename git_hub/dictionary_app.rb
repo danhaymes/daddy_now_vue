d0b06d740370e2661b13d004e3c1167f794445ee
Dan <dan.haymes@gmail.com>
2019-02-21 21:27:37 -0600

    loop for dictionary

--- git_hub/dictionary_app.rb
+++ git_hub/dictionary_app.rb
@@ -7,10 +7,23 @@
 
 require 'http'
 
+loop do 
  p "Please enter a word"
  word = gets.chomp
 
 
-response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=9f63510ab680a9a2504370c0a530c846ba454b12337f0c6c0")
+  response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=9f63510ab680a9a2504370c0a530c846ba454b12337f0c6c0")
 
-p response
+#p response
+
+  user_word = response.parse
+
+ 
+  definition = user_word[0]["text"]  
+  example_uses = user_word[0]["partOfSpeech"]
+  pronounciation = user_word[1]["attributionText"]
+  p definition
+  p example_uses
+  p pronounciation
+  word = gets.chomp
+ end 
